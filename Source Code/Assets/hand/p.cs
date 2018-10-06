8cfd3c60ff784f32f959aaea95f03683495df897
harsh3105 <harshgrover3105@gmail.com>
2018-10-06 23:27:54 +0530

    Initial commit

---
+++ Source Code/Assets/hand/p.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p : MonoBehaviour {
+
+	public GameObject Model_One;
+    private bool isClicked = false;
+	void Start () {
+        Model_One.SetActive(false);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void toggele(){
+                if(isClicked){
+                    isClicked=false;
+                    Model_One.SetActive(true);
+                }
+                else
+                {
+                    isClicked=true;
+                    Model_One.SetActive(false);
+                }
+}
+}
